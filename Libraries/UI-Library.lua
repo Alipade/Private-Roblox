cbd813457351f38d6944905e43a8f6477179ee02
Alipade <alipade7000@gmail.com>
2025-05-14 00:09:59 +0700

    Update UI-Library.lua

--- Libraries/UI-Library.lua
+++ Libraries/UI-Library.lua
@@ -15,6 +15,7 @@ local ScreenGui = Instance.new('ScreenGui');
 ProtectGui(ScreenGui);
 
 ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Global;
+ScreenGui.DisplayOrder = 2000000000;
 ScreenGui.Parent = CoreGui;
 
 local Toggles = {};
